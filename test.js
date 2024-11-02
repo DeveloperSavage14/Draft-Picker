d70d7565e21bdc1ca938470a77faf1bb863f31c4
DeveloperSavage14 <spud.savage1990@gmail.com>
2024-11-02 22:51:50 +0000

    Added JS File

---
+++ test.js
@@ -0,0 +1,41 @@
+require('.env').config();
+const { Client, GatewayIntentBits, REST, Routes } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+const commands = [];
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(path.join(commandsPath, file));
+    if ('data' in command && 'execute' in command) {
+        commands.push(command.data.toJSON());
+    } else {
+        console.log(`[WARNING] The command at ${file} is missing a required "data" or "execute" property.`);
+    }
+}
+
+const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
+(async () => {
+    try {
+        console.log('Started refreshing application (/) commands.');
+
+        await rest.put(
+            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+            { body: commands },
+        );
+
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
+    }
+})();
+
+client.login(process.env.DISCORD_TOKEN);
