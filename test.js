786440a1eb6748ac1fb91b2b4affef416127d8c2
modsadder <diegovantilborg@gmail.com>
2024-11-02 20:29:32 +0100

    added more choosing slots

--- test.js
+++ test.js
@@ -33,6 +33,15 @@ module.exports = {
         .setDescription("An example command")
         .addSubcommand(subcommand =>
             subcommand.setName("sub1").setDescription("Subcommand 1")
+
+        )
+        .addSubcommand(subcommand =>
+            subcommand.setName("sub2").setDescription("Subcommand 2")
+
+        )
+        .addSubcommand(subcommand =>
+            subcommand.setName("sub3").setDescription("Subcommand 3")
+
         )
 };
 
